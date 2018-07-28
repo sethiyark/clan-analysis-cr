dd3aecdece4437e4ae7649e224b56cd9fa73890d
Raunak Sethiya <sethiyark@gmail.com>
2018-07-28 11:48:16 +0530

    Created Basic Config for api

---
+++ scripts/main.py
@@ -0,0 +1,13 @@
+import requests
+
+from Config import *
+
+headers = {
+    'authorization': API_TOKEN,
+    'Accept': 'application/json'
+}
+
+response = requests.get(API_URL + '/' + CLAN_TAG, headers=headers)
+
+clan_info = response.json()
+print(clan_info)
